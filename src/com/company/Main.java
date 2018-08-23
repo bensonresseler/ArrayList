63671f380c1fee3efcfcfa390a7c89a50672fd69
bensonresseler <bresseler.vdab@gmail.com>
2018-08-23 11:07:21 +0200

    Oefn 4 - namen in bestand schrijven, daarna in arrayList opvangen.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,34 @@
 package com.company;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> namen = new ArrayList<>();
-        System.out.print("Geef naam (<RETURN> om te stoppen): ");
-        String naam = scanner.nextLine();
-        while(!(naam.equals(""))){
-            namen.add(naam);
+        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("namen.txt")))) {
             System.out.print("Geef naam (<RETURN> om te stoppen): ");
-            naam = (scanner.nextLine());
+            String naam = scanner.nextLine();
+            while (!naam.equals("")) {
+                writer.println(naam);
+                System.out.print("Geef naam (<RETURN> om te stoppen): ");
+                naam = scanner.nextLine();
+            }
         }
-        Collections.sort(namen);
-        for(String n: namen){
-            System.out.println(n);
-        }    }
+
+        System.out.println("Output: ");
+        ArrayList<String> namen = new ArrayList<>();
+        try (Scanner reader = new Scanner(new BufferedReader(new FileReader("namen.txt")))) {
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                namen.add(line);
+            }
+            Collections.sort(namen);
+            for (String n : namen) {
+                System.out.println(n);
+            }
+        }
+    }
 }
