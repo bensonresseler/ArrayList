268cd865ebfaf1b3cb5fda0ba1aa96a3438d05b7
bensonresseler <bresseler.vdab@gmail.com>
2018-08-23 10:20:50 +0200

    Oefn 2 - namen in ArrayList

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> namen = new ArrayList<>();
+        System.out.print("Geef naam (<RETURN> om te stoppen): ");
+        String naam = scanner.nextLine();
+        while(!(naam.equals(""))){
+            namen.add(naam);
+            System.out.print("Geef naam (<RETURN> om te stoppen): ");
+            naam = (scanner.nextLine());
+        }
+        Collections.sort(namen);
+        for(String n: namen){
+            System.out.println(n);
+        }    }
 }
